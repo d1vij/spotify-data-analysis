78ffffd6069f2cf68ba26b801d77e9aba8dbd897
d1vij <77372157+d1vij@users.noreply.github.com>
2025-08-29 07:45:09 +0000

    update

---
+++ generic_plots.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+class Plots:
+    @staticmethod
+    def plot_1(obj: pd.Series, rows, title, xlabel, ylabel):
+        __obj_slice = obj.iloc[:rows]
+
+        plt.figure(figsize=(15, 0.14 * rows))
+        plt.grid(axis='y', linestyle=':')
+
+        ax = sns.barplot(y=__obj_slice.index, x=__obj_slice.values, #type: ignore
+                        orient='h',
+                        width=0.8,
+                        palette="viridis",
+                        hue=__obj_slice.index) 
+
+        plt.xlabel(xlabel)
+        plt.xscale("log", base=np.e)
+
+
+        plt.ylabel(ylabel, rotation=0)
+        plt.tight_layout()
+
+        plt.grid(axis='both', linestyle=':')
+
+        for container in ax.containers:
+            plt.bar_label(container) #type: ignore
+
+        sns.despine(top=True, bottom=True, right=True, left=True)
+
+        plt.title(title)
+        plt.show()
+
+    @staticmethod
+    def plot_2(y:pd.Series, title:str, xlabel, ylabel):
+        plt.figure(figsize=(7,5))
+        ax = sns.barplot(x=y.index, y=y.values, palette="pastel", hue=y.index) #type: ignore
+        
+        for container in ax.containers:
+            plt.bar_label(container) #type: ignore
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        
+        plt.grid(linestyle=":", axis="y")
+        
+        sns.despine(top=True, right=True)
+        plt.tight_layout()
+        plt.show()
---
+++ preprocessing.py
@@ -0,0 +1,91 @@
+import json
+from typing import TypedDict, Literal, Optional, cast
+
+
+class TrackInfo(TypedDict, total=True):
+    ts: str
+    platform: str
+    ms_played: int
+    conn_country: str
+    ip_addr: str
+    master_metadata_track_name: str
+    master_metadata_album_artist_name: str
+    master_metadata_album_album_name: str
+    spotify_track_uri: str
+    episode_name: Optional[str]
+    episode_show_name: Optional[str]
+    spotify_episode_uri: Optional[str]
+    audiobook_title: Optional[str]
+    audiobook_uri: Optional[str]
+    audiobook_chapter_uri: Optional[str]
+    audiobook_chapter_title: Optional[str]
+    reason_start: Optional[
+        Literal[
+            "playbtn", "fwdbtn", "backbtn", "trackdone",
+            "clickrow", "appload", "remote", "trackerror", "unknown"
+        ]
+    ]
+    reason_end: Optional[
+        Literal[
+            "fwdbtn", "backbtn", "logout", "endplay",
+            "trackdone", "unknown", "remote",
+            "unexpected-exit-while-paused", "unexpected-exit", "trackerror"
+        ]
+    ]
+    shuffle: bool
+    skipped: bool
+    offline: bool
+    offline_timestamp: int
+    incognito_mode: bool
+
+    
+class FilteredTrackInfo(TypedDict):
+    ts: Optional[str]
+    platform: Optional[str]
+    ms_played: Optional[int]
+    conn_country: Optional[str]
+    ip_addr: Optional[str]
+    master_metadata_track_name: Optional[str]
+    master_metadata_album_artist_name: Optional[str]
+    master_metadata_album_album_name: Optional[str]
+    spotify_track_uri: Optional[str]
+    episode_name: Optional[str]
+    episode_show_name: Optional[str]
+    spotify_episode_uri: Optional[str]
+    audiobook_title: Optional[str]
+    audiobook_uri: Optional[str]
+    audiobook_chapter_uri: Optional[str]
+    audiobook_chapter_title: Optional[str]
+    reason_start: Optional[
+        Literal[
+            "playbtn", "fwdbtn", "backbtn", "trackdone", 
+            "clickrow", "appload", "remote", "trackerror", "unknown"
+        ]
+    ]
+    reason_end: Optional[
+        Literal[
+            "fwdbtn", "backbtn", "logout", "endplay", "trackdone", 
+            "unknown", "remote", "unexpected-exit-while-paused", 
+            "unexpected-exit", "trackerror"
+        ]
+    ]
+    shuffle: Optional[bool]
+    skipped: Optional[bool]
+    offline: Optional[bool]
+    offline_timestamp: Optional[int]
+    incognito_mode: Optional[bool]
+
+SongInfoArray = list[TrackInfo]
+FilteredSongInfoArray = list[FilteredTrackInfo]
+SongAttributes = Literal["ts","platform","ms_played","conn_country","ip_addr","master_metadata_track_name","master_metadata_album_artist_name","master_metadata_album_album_name","spotify_track_uri","episode_name","episode_show_name","spotify_episode_uri","audiobook_title","audiobook_uri","audiobook_chapter_uri","audiobook_chapter_title","reason_start","reason_end","shuffle","skipped","offline","offline_timestamp","incognito_mode"]
+
+def read_file(name:str) -> SongInfoArray:
+    with open(name, 'r') as file:
+        return json.loads(file.read())
+
+def extract_filtered(song_info_obj,filters:list[SongAttributes]) -> FilteredTrackInfo:
+    filtered = {}
+    for filter in filters : filtered[filter] = song_info_obj[filter]
+    return cast(FilteredTrackInfo, filtered)
+    
+    
