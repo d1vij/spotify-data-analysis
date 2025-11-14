95a395dde38b12b7d330df2320ad16a5eee56746
d1vij <77372157+d1vij@users.noreply.github.com>
2025-11-14 19:51:34 +0530

    Add conversion of notebook to html in the export pipeline inside notebook and refactor export.sh to export.py

---
+++ scripts/export.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# .venv/bin/python3 -m jupyter nbconvert --to html --no-prompt project.ipynb --output-dir="./build"
+
+
+if (__name__ == "__main__"):
+    subprocess.run(["jupyter", "nbconvert", "--to", "html", "--no-prompt", "project.ipynb", "--output-dir=\"./build\""])
--- scripts/export.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-.venv/bin/python3 -m jupyter nbconvert --to html --no-prompt project.ipynb --output-dir="./build"
