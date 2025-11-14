b7c9540ebf056786a77a67afd5e4d290090899a5
d1vij <77372157+d1vij@users.noreply.github.com>
2025-11-14 20:05:44 +0530

    Fix build script

--- scripts/export.py
+++ scripts/export.py
@@ -4,4 +4,4 @@ import subprocess
 
 
 if (__name__ == "__main__"):
-    subprocess.run(["jupyter", "nbconvert", "--to", "html", "--no-prompt", "project.ipynb", "--output-dir=\"./build\""])
+    subprocess.run(["jupyter", "nbconvert", "--to", "html", "--no-prompt", "project.ipynb", "--output-dir=\"./build\""], cwd=".")
